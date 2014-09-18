521cac492030c67a21b6a72462641a6f3d9921cb
Stephen Young <StephenJayYoung@gmail.com>
2014-09-18 14:52:11 -0700

    wizzzzardry.

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+/**
+ * @typedef {Object} Coordinate
+ * @property {Number} row - The row
+ * @property {Number} col - The column
+ */
+
 /* This checks to see if there is a number in that space.
  *
  * @function isThereANumberInThatSpace
  *
  * @param {string} theSudokuString the string of Sudoku numbers i.e. the puzzle.
- * @param {?} the location of the cell to be checked.
+ * @param {Coordinate} the location of the cell to be checked.
  *
  * @returns {boolean} the existence of the number is either true or false.
  * 
--- test/test.js
+++ test/test.js
@@ -9,7 +9,7 @@ var lib = require('../index');
 describe("isThereANumberInThatSpace()", function() {
 	it('verifies that there is a number at that given coordinate', function() {
 		var theSudokuString = '158 2  6 2   8  9  3  7 8 2 6 74      4 6 7      19 5 4 9 3  2  2  5   8 7  9 413';
-		var coordinate = 'wizardshizzle';
+		var coordinate = { row: 0, col: 0 };
 		expect(lib.isThereANumberInThatSpace(theSudokuString, coordinate)).to.eql(true);
 	});
 });
