020e7810f6b04c3468332152e933ac7b64f70bf9
Stephen Young <StephenJayYoung@gmail.com>
2014-09-18 14:07:07 -0700

    created a working test

---
+++ test/test.js
@@ -0,0 +1,18 @@
+var chai = require("chai");
+var expect = chai.expect;
+var lib = require('../index');
+
+
+//
+
+
+describe("isThereANumberInThatSpace()", function() {
+	it('checks to see if thre is s numbet', function() {
+		expect(SudokuSolver()).to.eql(true);
+	});
+});
+
+
+
+
+
