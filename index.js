c4671b9e4ff0c42a754864649b37e00080c4c316
Stephen Young <StephenJayYoung@gmail.com>
2014-09-18 14:48:26 -0700

    added working tests, paramaters, started working on index

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// this is our index fileS
+/* This checks to see if there is a number in that space.
+ *
+ * @function isThereANumberInThatSpace
+ *
+ * @param {string} theSudokuString the string of Sudoku numbers i.e. the puzzle.
+ * @param {?} the location of the cell to be checked.
+ *
+ * @returns {boolean} the existence of the number is either true or false.
+ * 
+ */
+
+module.exports.isThereANumberInThatSpace = function(theSudokuString, coordinate) {
+
+
+};
--- test/test.js
+++ test/test.js
@@ -7,8 +7,10 @@ var lib = require('../index');
 
 
 describe("isThereANumberInThatSpace()", function() {
-	it('checks to see if thre is s numbet', function() {
-		expect(SudokuSolver()).to.eql(true);
+	it('verifies that there is a number at that given coordinate', function() {
+		var theSudokuString = '158 2  6 2   8  9  3  7 8 2 6 74      4 6 7      19 5 4 9 3  2  2  5   8 7  9 413';
+		var coordinate = 'wizardshizzle';
+		expect(lib.isThereANumberInThatSpace(theSudokuString, coordinate)).to.eql(true);
 	});
 });
 
